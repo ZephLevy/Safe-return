3259df0a5beb16905616f1e7f4e67ee9263bccaf
Zeph <write2zeph@gmail.com>
2025-01-09 17:03:56 +0100

    Added a timeLeft widget and an SOS button. The timeLeft widget doesn't show the time left and the SOS button doesn't do anything yet.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,50 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      floatingActionButton: _sosButton(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _timeLeft()
+        ],
+      )
+    );
+  }
+
+  FloatingActionButton _sosButton() {
+    return FloatingActionButton.large(
+      onPressed: () {},
+      backgroundColor: Color(0xffB20000),
+      child: Icon(
+        Icons.sos,
+        size: 55,
+        color: Palette.blue1,
+      ),
+    );
+  }
+
+  Widget _timeLeft() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      width: double.infinity,
+      child: Card(
+        color: Color(0xffD6D6D6), //TODO: make this a different color when user is on trip
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20) 
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Center(
+            child: Text(
+              'Time left: []',
+              style: TextStyle(fontSize: 18)
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
