6b704d2da66515aca83da20b31f21060b26b62dd
Riccaerdo <riccardo.pertusio10@gmail.com>
2025-01-31 21:32:47 +0100

    trying out the settings page to see if everything works fine

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -5,6 +5,16 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Example")),
+        body: Center(
+            child: Container(
+                width: 100,
+                height: 100,
+                margin: EdgeInsets.all(20),
+                color: const Color.fromARGB(255, 42, 77, 255))),
+      ),
+    );
   }
 }
