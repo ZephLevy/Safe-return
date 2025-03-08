200ad014a566affffbb35feef484c8e450cb2f75
Grayerhack700 <riccardo.pertusio10@icloud.com>
2025-03-08 12:38:07 +0100

    Add permission to access contacts and add new color palette

--- lib/palette.dart
+++ lib/palette.dart
@@ -6,5 +6,6 @@ class Palette {
   static const Color blue3 = Color(0xff65a4a3);
   static const Color blue4 = Color(0xffa4d2d5);
   static const Color yellow = Color(0xfff9e79f);
-  static const colorList = [blue1, blue2, blue3, blue4, yellow];
+  static const Color purple = Color.fromARGB(255, 71, 52, 133);
+  static const colorList = [blue1, blue2, blue3, blue4, yellow, purple];
 }
