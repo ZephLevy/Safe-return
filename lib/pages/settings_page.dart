1c01df5483a42bba2f16551aa2f22fbb6c1ca16b
Zeph <write2zeph@gmail.com>
2025-03-21 18:18:45 +0100

    Add support for a popup menu for entering a code, saving it in the sosManager class
    This is not implemented anywhere yet

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -334,4 +334,38 @@ class OptionsState extends State<Options> {
           style: TextStyle(fontSize: 16),
         ),
       );
+
+  static void showCodeEnter(BuildContext context) {
+    TextEditingController textController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Enter a code"),
+          content: TextField(
+            controller: textController,
+            decoration: InputDecoration(hintText: "Enter your secret code..."),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close dialog
+              },
+              child: Text(
+                "Cancel",
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                SosManager.secretCode = textController.text;
+                Navigator.of(context).pop(); // Close dialog
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
